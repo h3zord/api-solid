f96242969f1a0a1afb1227f4d9d3578a2cb6efb9
Lucas <lucaschmain@gmail.com>
2024-09-26 16:21:39 -0300

    auth: adding jwt token

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from 'fastify'
+import { authenticate } from '@/http/controllers/authenticate'
 import { register } from './controllers/register'
-import { authenticate } from './controllers/authenticate'
+import { profile } from '@/http/controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', profile)
 }
